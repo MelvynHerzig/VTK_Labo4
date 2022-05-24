56a9f00fb4d5299557c4db0e4d1b10c5015daa3f
Herzig Melvyn <melvyn.herzig@heig-vd.ch>
2022-05-24 11:28:31 +0200

    init

---
+++ Labo4_Forestier_Herzig.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# Labo 4 from VTK.
+#
+# Goal: The goal of this laboratory is to visualize the path of a glider taking off 
+#       from the frozen lake of Ottsjön in Sweden. The flight of this glider will also
+#       allow us to visualize the ascending and descending airflows under the influence 
+#       of the surrounding mountain relief. 
+#
+# Authors: Forestier Quentin & Herzig Melvyn
+#
+# Date: 24.05.2022
+
+import vtk
